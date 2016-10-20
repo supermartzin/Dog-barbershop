5e1003fa5ffd26a2d8861161b2f5ea2bbfbf6021
Martin Vrábel <supermartzin@gmail.com>
2016-10-21 00:12:24 +0200

    Project initialization, setup POM, added Hibernate JPA library and Spring config, first classes

---
+++ src/main/java/cz/muni/fi/pa165/dao/EmployeeDAO.java
@@ -0,0 +1,8 @@
+package cz.muni.fi.pa165.dao;
+
+/**
+ * @author Martin Vrábel
+ * @version 21.10.2016 0:00
+ */
+public interface EmployeeDAO {
+}
---
+++ src/main/java/cz/muni/fi/pa165/entities/User.java
@@ -0,0 +1,8 @@
+package cz.muni.fi.pa165.entities;
+
+/**
+ * @author Martin Vrábel
+ * @version 20.10.2016 23:59
+ */
+public class User {
+}
