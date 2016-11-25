0275ed826cd9779f819afd3bcee0c18392ffa815
Linzee <d.gmiterko@gmail.com>
2016-11-25 16:16:30 +0100

    Implement CustomerService

--- service/src/main/java/cz/muni/fi/pa165/service/CustomerService.java
+++ service/src/main/java/cz/muni/fi/pa165/service/CustomerService.java
@@ -1,6 +1,7 @@
 package cz.muni.fi.pa165.service;
 
 import cz.muni.fi.pa165.entities.Customer;
+import cz.muni.fi.pa165.exceptions.DAOException;
 
 import java.util.List;
 
@@ -17,8 +18,9 @@ public interface CustomerService {
      *
      * @param customer                  {@link Customer} object to save
      * @throws IllegalArgumentException when {@code customer} is {@code null}
+     * @throws DAOException             when problem happens on underlying layer
      */
-    void create(Customer customer);
+    void create(Customer customer) throws DAOException;
 
     /**
      * Retrieves a {@link Customer} object with provided <b>ID</b>.
@@ -26,6 +28,7 @@ public interface CustomerService {
      * @param id                        <b>ID</b> number of {@link Customer} to retrieve
      * @return                          found {@link Customer} object or {@code null} if object with specified <b>ID</b> not found
      * @throws IllegalArgumentException when {@code id} is a negative number
+     * @throws DAOException             when problem happens on underlying layer
      */
     Customer getById(long id);
 
@@ -35,29 +38,33 @@ public interface CustomerService {
      * @param username                  username of {@link Customer} to retrieve
      * @return                          found {@link Customer} object or {@link null} if <b>username</b> not found
      * @throws IllegalArgumentException when {@code username} is {@code null
+     * @throws DAOException             when problem happens on underlying layer
      */
-    Customer getByUsername(String username);
+    Customer getByUsername(String username) throws DAOException;
 
     /**
      * Retrieves all {@link Customer} objects.
      *
      * @return              list with all {@link Customer} objects or <b>empty list</b> if there are no entries
+     * @throws DAOException             when problem happens on underlying layer
      */
-    List<Customer> getAll();
+    List<Customer> getAll() throws DAOException;
 
     /**
      * Updates attributes of an existing {@link Customer} object.
      *
      * @param customer                  {@link Customer} object with updated attributes
      * @throws IllegalArgumentException when {@code customer} is {@code null}
+     * @throws DAOException             when problem happens on underlying layer
      */
-    void update(Customer customer);
+    void update(Customer customer) throws DAOException;
 
     /**
      * Deletes an existing {@link Customer} entry from database.
      *
      * @param customer                  {@link Customer} object to delete from database
      * @throws IllegalArgumentException when {@code customer} is {@code null}
+     * @throws DAOException             when problem happens on underlying layer
      */
-    void delete(Customer customer);
+    void delete(Customer customer) throws DAOException;
 }
--- service/src/main/java/cz/muni/fi/pa165/service/CustomerServiceImpl.java
+++ service/src/main/java/cz/muni/fi/pa165/service/CustomerServiceImpl.java
@@ -1,7 +1,11 @@
 package cz.muni.fi.pa165.service;
 
+import cz.muni.fi.pa165.dao.CustomerDAO;
+import cz.muni.fi.pa165.dao.EmployeeDAO;
 import cz.muni.fi.pa165.entities.Customer;
+import cz.muni.fi.pa165.exceptions.DAOException;
 
+import javax.inject.Inject;
 import java.util.List;
 
 /**
@@ -11,33 +15,36 @@ import java.util.List;
  */
 public class CustomerServiceImpl implements CustomerService {
 
-    @Override
-    public void create(Customer customer) {
+    @Inject
+    private CustomerDAO customerDAO;
 
+    @Override
+    public void create(Customer customer) throws DAOException {
+        customerDAO.create(customer);
     }
 
     @Override
     public Customer getById(long id) {
-        return null;
+        return customerDAO.getById(id);
     }
 
     @Override
-    public Customer getByUsername(String username) {
-        return null;
+    public Customer getByUsername(String username) throws DAOException {
+        return customerDAO.getByUsername(username);
     }
 
     @Override
-    public List<Customer> getAll() {
-        return null;
+    public List<Customer> getAll() throws DAOException {
+        return customerDAO.getAll();
     }
 
     @Override
-    public void update(Customer customer) {
-
+    public void update(Customer customer) throws DAOException {
+        customerDAO.update(customer);
     }
 
     @Override
-    public void delete(Customer customer) {
-
+    public void delete(Customer customer) throws DAOException {
+        customerDAO.delete(customer);
     }
 }
