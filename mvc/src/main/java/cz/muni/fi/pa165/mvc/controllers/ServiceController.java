f729df3d7e306aae851dabf1aa9489199e76aded
Linzee <d.gmiterko@gmail.com>
2016-12-16 15:05:38 +0100

    Service controller

--- mvc/src/main/java/cz/muni/fi/pa165/mvc/controllers/ServiceController.java
+++ mvc/src/main/java/cz/muni/fi/pa165/mvc/controllers/ServiceController.java
@@ -2,12 +2,12 @@ package cz.muni.fi.pa165.mvc.controllers;
 
 import cz.muni.fi.pa165.exceptions.DAOException;
 import cz.muni.fi.pa165.facade.CustomerFacade;
+import cz.muni.fi.pa165.facade.ServiceFacade;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,23 +15,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * @author Dominik Gmiterko
  */
 @Controller
-@RequestMapping("/customer")
-public class CustomerController {
+@RequestMapping("/service")
+public class ServiceController {
 
-    final static Logger log = LoggerFactory.getLogger(CustomerController.class);
+    final static Logger log = LoggerFactory.getLogger(ServiceController.class);
 
     @Autowired
-    private CustomerFacade customerFacade;
+    private ServiceFacade serviceFacade;
 
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public String list(Model model) throws DAOException {
-        model.addAttribute("customers", customerFacade.getAll());
-        return "customer/list";
-    }
-
-    @RequestMapping(value = "/detail/{id}", method = RequestMethod.GET)
-    public String detail(@PathVariable long id, Model model) {
-        model.addAttribute("customer", customerFacade.getById(id));
-        return "customer/detail";
+        model.addAttribute("services", serviceFacade.getAll());
+        return "service/list";
     }
 }
---
+++ mvc/src/main/webapp/WEB-INF/jsp/service/list.jsp
@@ -0,0 +1,25 @@
+<%@ page contentType="text/html;charset=UTF-8" pageEncoding="utf-8" trimDirectiveWhitespaces="false" session="false" %>
+<%@ taglib tagdir="/WEB-INF/tags" prefix="my" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib prefix="s" uri="http://www.springframework.org/tags" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+
+<my:pagetemplate title="Services">
+<jsp:attribute name="body">
+
+    <div class="row">
+        <c:forEach items="${services}" var="service">
+        <div class="col-sm-3 col-md-3">
+            <div class="well">
+                <h2 class="text-muted"><c:out value="${service.title}"/></h2>
+                <p class="price">Length of service: <c:out value="${service.length}"/> minutes</p>
+                <hr>
+                <h3 class="price"><c:out value="${service.price}"/></h3>
+            </div>
+        </div>
+        </c:forEach>
+    </div>
+
+</jsp:attribute>
+</my:pagetemplate>
