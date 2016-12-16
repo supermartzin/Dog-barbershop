f9a4c7c00239c2e192037f0f2d5e6a140cefd2f0
Denis Richtárik <denisricht@gmail.com>
2016-12-16 19:39:32 +0100

    DogFacade and EmployeeFacade tests

--- service/src/test/java/cz/muni/fi/pa165/facade/DogFacadeTest.java
+++ service/src/test/java/cz/muni/fi/pa165/facade/DogFacadeTest.java
@@ -1,54 +1,156 @@
 package cz.muni.fi.pa165.facade;
 
-import org.junit.After;
+import cz.muni.fi.pa165.dto.DogDTO;
+import cz.muni.fi.pa165.entities.Address;
+import cz.muni.fi.pa165.entities.Customer;
+import cz.muni.fi.pa165.entities.Dog;
+import cz.muni.fi.pa165.exceptions.FacadeException;
+import cz.muni.fi.pa165.service.BeanMappingService;
+import cz.muni.fi.pa165.service.DogService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Tests for correct contract implementation defined by {@link DogFacade} interface
  *
- * @author Martin Vrábel
+ * @author Denis Richtarik
  * @version 22.11.2016 14:09
  */
 public class DogFacadeTest {
 
+    @Mock
+    private DogService dogService;
+
+    @Mock
+    private BeanMappingService mappingService;
+
+    private DogFacadeImpl dogFacade;
+    private DogDTO dogDTO;
+    private Dog testingDog;
+    private Customer testingCustomer;
+    private Address address;
+
+
     @Before
     public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        dogFacade = new DogFacadeImpl(dogService, mappingService);
+        dogDTO = new DogDTO("Doge", "pepe", 20);
+        address = new Address("Testing Avenue", 25, "Testero", 2356, "Testing Republic");
+        testingCustomer = new Customer("testing", "password", "John", "Tester", address,
+                "testing.employee@mail.com", "755468236");
+        testingDog = new Dog("Doge", "pepe", 20, testingCustomer);
 
     }
 
-    @After
-    public void tearDown() throws Exception {
-
-    }
-
-    @Test
-    public void create() throws Exception {
-
-    }
-
-    @Test
-    public void getById() throws Exception {
-
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreate_null() throws Exception {
+        dogFacade.create(null);
     }
 
     @Test
-    public void getAll() throws Exception {
+    public void testCreate() throws Exception {
+        when(mappingService.mapTo(dogDTO, Dog.class)).thenReturn(testingDog);
 
+        dogFacade.create(dogDTO);
+
+        verify(mappingService, times(1)).mapTo(dogDTO, Dog.class);
+        verify(dogService, times(1)).create(testingDog);
     }
 
     @Test
-    public void getByCustomer() throws Exception {
+    public void testGetById() throws Exception {
+        when(mappingService.mapTo(testingDog, DogDTO.class)).thenReturn(dogDTO);
+        when(dogService.getById(anyLong())).thenReturn(testingDog);
 
+        DogDTO result = dogFacade.getById(1);
+
+        assertNotNull(result);
+        assertEquals(dogDTO, result);
+
+        verify(mappingService, times(1)).mapTo(testingDog, DogDTO.class);
     }
 
     @Test
-    public void update() throws Exception {
+    public void testGetAll() throws Exception {
+        Dog testingDog1 = new Dog("Doggo", "lele", 22, testingCustomer);
+        DogDTO dogDTO1 = new DogDTO("Doggo", "lele", 22);
 
+        when(dogService.getAll()).thenReturn(Arrays.asList(testingDog, testingDog1));
+        when(mappingService.mapTo(Arrays.asList(testingDog, testingDog1), DogDTO.class))
+                .thenReturn(Arrays.asList(dogDTO, dogDTO1));
+
+        List<DogDTO> result = dogFacade.getAll();
+
+        assertNotNull(result);
+        assertEquals(2, result.size());
+        assertThat(result, hasItems(dogDTO, dogDTO1));
+
+        verify(dogService, times(1)).getAll();
+        verify(mappingService, times(1)).mapTo(anyCollectionOf(Dog.class), eq(DogDTO.class));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testUpdate_null() throws FacadeException {
+        dogFacade.update(null);
     }
 
     @Test
-    public void delete() throws Exception {
+    public void testUpdate() throws Exception {
+        Dog updatedDog = new Dog("Doge", "pepe", 22, testingCustomer);
+        DogDTO updatedDogDTO = new DogDTO("Doge", "pepe", 22);
 
+        when(mappingService.mapTo(updatedDogDTO, Dog.class)).thenReturn(updatedDog);
+        when(mappingService.mapTo(updatedDog, DogDTO.class)).thenReturn(updatedDogDTO);
+        when(dogService.getById(anyLong())).thenReturn(updatedDog);
+
+        dogFacade.update(updatedDogDTO);
+
+        DogDTO updatedEmployeeDTO = dogFacade.getById(1);
+
+        assertNotNull(updatedEmployeeDTO);
+        assertEquals(updatedDogDTO, updatedEmployeeDTO);
+
+        verify(mappingService, times(1)).mapTo(updatedDogDTO, Dog.class);
+        verify(mappingService, times(1)).mapTo(updatedDog, DogDTO.class);
+        verify(dogService, times(1)).update(updatedDog);
+        verify(dogService, times(1)).getById(anyLong());
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDelete_null() throws FacadeException {
+        dogFacade.delete(null);
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        when(mappingService.mapTo(dogDTO, Dog.class)).thenReturn(testingDog);
+        when(mappingService.mapTo(anyCollectionOf(Dog.class), eq(DogDTO.class))).thenReturn(Collections.emptyList());
+        when(dogService.getAll()).thenReturn(Collections.emptyList());
+
+        dogFacade.delete(dogDTO);
+
+        List<DogDTO> remainingServices = dogFacade.getAll();
+
+        assertNotNull(remainingServices);
+        assertTrue(remainingServices.isEmpty());
+        assertThat(remainingServices, not(hasItem(dogDTO)));
+    }
+    
 }
--- service/src/test/java/cz/muni/fi/pa165/facade/EmployeeFacadeTest.java
+++ service/src/test/java/cz/muni/fi/pa165/facade/EmployeeFacadeTest.java
@@ -1,54 +1,177 @@
 package cz.muni.fi.pa165.facade;
 
-import org.junit.After;
+import cz.muni.fi.pa165.dto.AddressDTO;
+import cz.muni.fi.pa165.dto.EmployeeDTO;
+import cz.muni.fi.pa165.entities.Address;
+import cz.muni.fi.pa165.entities.Employee;
+import cz.muni.fi.pa165.exceptions.FacadeException;
+import cz.muni.fi.pa165.service.BeanMappingService;
+import cz.muni.fi.pa165.service.EmployeeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Tests for correct contract implementation defined by {@link EmployeeFacade} interface
  *
- * @author Martin Vrábel
+ * @author Denis Richtarik
  * @version 22.11.2016 14:10
  */
 public class EmployeeFacadeTest {
 
+    @Mock
+    private EmployeeService employeeService;
+
+    @Mock
+    private BeanMappingService mappingService;
+
+    private EmployeeFacadeImpl employeeFacade;
+    private EmployeeDTO employeeDTO;
+    private Employee testingEmployee;
+    private AddressDTO addressDTO;
+    private Address address;
+
+
     @Before
     public void setUp() throws Exception {
-
+        MockitoAnnotations.initMocks(this);
+        employeeFacade = new EmployeeFacadeImpl(employeeService, mappingService);
+        addressDTO = new AddressDTO("Testing Avenue", 25, "Testero", 2356, "Testing Republic");
+        employeeDTO = new EmployeeDTO("testing", "password", "John", "Tester", addressDTO,
+                "testing.employee@mail.com", "755468236", new BigDecimal("4200"));
+        address = new Address("Testing Avenue", 25, "Testero", 2356, "Testing Republic");
+        testingEmployee = new Employee("testing", "password", "John", "Tester", address,
+                "testing.employee@mail.com", "755468236", new BigDecimal("4200"));
     }
 
-    @After
-    public void tearDown() throws Exception {
-
-    }
-
-    @Test
-    public void create() throws Exception {
-
-    }
-
-    @Test
-    public void getById() throws Exception {
-
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreate_null() throws Exception {
+        employeeFacade.create(null);
     }
 
     @Test
-    public void getAll() throws Exception {
+    public void testCreate() throws Exception {
+        when(mappingService.mapTo(employeeDTO, Employee.class)).thenReturn(testingEmployee);
 
+        employeeFacade.create(employeeDTO);
+
+        verify(mappingService, times(1)).mapTo(employeeDTO, Employee.class);
+        verify(employeeService, times(1)).create(testingEmployee);
     }
 
     @Test
-    public void getByUsername() throws Exception {
+    public void testGetById() throws Exception {
+        when(mappingService.mapTo(testingEmployee, EmployeeDTO.class)).thenReturn(employeeDTO);
+        when(employeeService.getById(anyLong())).thenReturn(testingEmployee);
 
+        EmployeeDTO result = employeeFacade.getById(1);
+
+        assertNotNull(result);
+        assertEquals(employeeDTO, result);
+
+        verify(mappingService, times(1)).mapTo(testingEmployee, EmployeeDTO.class);
     }
 
     @Test
-    public void update() throws Exception {
+    public void testGetAll() throws Exception {
+        Employee testingEmployee2 = new Employee("tetete", "tititi", "tatata", "tututu", address,
+                "tute@toto.com", "755468236", new BigDecimal("2313"));
+        EmployeeDTO employeeDTO1 = new EmployeeDTO("tetete", "tititi", "tatata", "tututu", addressDTO,
+                "tute@toto.com", "755468236", new BigDecimal("2313"));
 
+        when(employeeService.getAll()).thenReturn(Arrays.asList(testingEmployee, testingEmployee2));
+        when(mappingService.mapTo(Arrays.asList(testingEmployee, testingEmployee2), EmployeeDTO.class))
+                .thenReturn(Arrays.asList(employeeDTO, employeeDTO1));
+
+        List<EmployeeDTO> result = employeeFacade.getAll();
+
+        assertNotNull(result);
+        assertEquals(2, result.size());
+        assertThat(result, hasItems(employeeDTO, employeeDTO1));
+
+        verify(employeeService, times(1)).getAll();
+        verify(mappingService, times(1)).mapTo(anyCollectionOf(Employee.class), eq(EmployeeDTO.class));
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetByUsername_null() throws FacadeException {
+        employeeFacade.getByUsername(null);
+    }
+    
     @Test
-    public void delete() throws Exception {
+    public void testGetByUsername() throws Exception {
+        when(mappingService.mapTo(testingEmployee, EmployeeDTO.class)).thenReturn(employeeDTO);
+        when(employeeService.getByUsername(any())).thenReturn(testingEmployee);
 
+        EmployeeDTO result = employeeFacade.getByUsername("testing");
+
+        assertNotNull(result);
+        assertEquals(employeeDTO, result);
+
+        verify(mappingService, times(1)).mapTo(testingEmployee, EmployeeDTO.class);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testUpdate_null() throws FacadeException {
+        employeeFacade.update(null);
+    }
+    
+    @Test
+    public void testUpdate() throws Exception {
+        EmployeeDTO updatedServiceDTO = new EmployeeDTO("tetete", "tititi", "tatata", "tututu", addressDTO,
+                "tute@toto.com", "755468236", new BigDecimal("2313"));
+        Employee updatedEmployee = new Employee("tetete", "tititi", "tatata", "tututu", address,
+                "tute@toto.com", "755468236", new BigDecimal("2313"));
+
+        when(mappingService.mapTo(updatedServiceDTO, Employee.class)).thenReturn(updatedEmployee);
+        when(mappingService.mapTo(updatedEmployee, EmployeeDTO.class)).thenReturn(updatedServiceDTO);
+        when(employeeService.getById(anyLong())).thenReturn(updatedEmployee);
+
+        employeeFacade.update(updatedServiceDTO);
+
+        EmployeeDTO updatedEmployeeDTO = employeeFacade.getById(1);
+
+        assertNotNull(updatedEmployeeDTO);
+        assertEquals(updatedServiceDTO, updatedEmployeeDTO);
+
+        verify(mappingService, times(1)).mapTo(updatedServiceDTO, Employee.class);
+        verify(mappingService, times(1)).mapTo(updatedEmployee, EmployeeDTO.class);
+        verify(employeeService, times(1)).update(updatedEmployee);
+        verify(employeeService, times(1)).getById(anyLong());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDelete_null() throws FacadeException {
+        employeeFacade.delete(null);
+    }
+    
+    @Test
+    public void testDelete() throws Exception {
+        when(mappingService.mapTo(employeeDTO, Employee.class)).thenReturn(testingEmployee);
+        when(mappingService.mapTo(anyCollectionOf(Employee.class), eq(EmployeeDTO.class))).thenReturn(Collections.emptyList());
+        when(employeeService.getAll()).thenReturn(Collections.emptyList());
+
+        employeeFacade.delete(employeeDTO);
+
+        List<EmployeeDTO> remainingServices = employeeFacade.getAll();
+
+        assertNotNull(remainingServices);
+        assertTrue(remainingServices.isEmpty());
+        assertThat(remainingServices, not(hasItem(employeeDTO)));
     }
 }
